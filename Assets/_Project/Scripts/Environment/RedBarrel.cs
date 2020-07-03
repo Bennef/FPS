c7502ccb678ade5ca1de4a0988c3800ca5b00795
Ben <benneftobennef@hotmail.com>
2020-07-03 11:42:10 +0100

    Added red barrel explosion functionality. Added camera shake on explosion. Artifacts from explosion need looking at.

---
+++ Assets/_Project/Scripts/Environment/RedBarrel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Scripts.Environment
+{
+    public class RedBarrel : MonoBehaviour
+    {
+        [SerializeField] private GameObject _explosion;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Laser Shot"))
+                Explode();
+        }
+
+        void Explode()
+        {
+            GameObject tmp = Instantiate<GameObject>(_explosion, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
